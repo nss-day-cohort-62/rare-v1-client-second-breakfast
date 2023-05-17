78b8add122d1e7cbaaa8dc71bc27be3f1e8e0b76
Dylan Kline <dylanekline@gmail.com>
2023-05-17 09:42:06 -0500

    JSON for category view

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
 import { Authorized } from "./Authorized"
+import { AuthorSubscription } from "../components/author/AuthorSubscriptions"
 
 export const ApplicationViews = ({ token, setToken }) => {
   return <>
@@ -10,7 +11,7 @@ export const ApplicationViews = ({ token, setToken }) => {
       <Route path="/register" element={<Register setToken={setToken} />}  />
       <Route element={<Authorized token={token} />}>
         {/* Add Routes here */}
-        
+        <Route path="/" element={<AuthorSubscription />} />
       </Route>
     </Routes>
   </>
